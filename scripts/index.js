e87e0bb5f82357c740b21ecff44b2235198a98bf
julie1013 <julie1380@verizon.net>
2016-09-05 22:05:49 -0400

    Win conditions working
    Tested functions that test for win conditions. Made sure that each win condition
    was tested and returns true.

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,8 @@
 let board = [];
 let whoseTurn = 0;
+let xCount = 0;
+let oCount = 0;
+let winner;
 
 const initializeBoard = function(){
   for (let i = 0; i < 9; i++){
@@ -28,7 +31,7 @@ const isO = function(){
 };
 //is player o
 
-const isPlayerTurn = function(player){
+const isPlayerTurn = function(){
   if (whoseTurn === 0){
     player = 'x';
   } else {
@@ -61,68 +64,72 @@ const selectSquare = function(element){
 const play = function(cell){
     player = isPlayerTurn();
     setCell(cell, player);
-    turnSwitch();
+    turnCount();
+    if(winCheck()){
+      alert('You win!');
+    } else {
+      turnSwitch();
+    }
 };
 //player is able to play their token in a cell
 
 const setCell = function(cell, player){
-  $('#' + cell).html('<img src = ./images/'+ player +'.png style="width: 90px">').removeClass('unoccupied').removeClass('selected').addClass('occupied');
+  $('#' + cell).html('<img src = ./images/'+ player +'.png style="width: 90px">').removeClass('unoccupied').removeClass('selected').addClass('occupied').off();
+  board[cell] = player;
 };
 //visually updates cell with player token
 
-const horizontalAdjacent = function(firstCell, adjacentCellOne, adjacentCellTwo){
-  if (Math.abs(adjacentCellOne - firstCell) === 1){
-    if(Math.abs(adjacentCellTwo - firstCell) === 1 || Math.abs(adjacentCellTwo - firstCell) === 2){
-      return true;
-    } else {
-      return false;
-    }
+const turnCount = function(){
+  if(whoseTurn === 0){
+    xCount++;
+  } else {
+    oCount++;
   }
-  return false;
+};
+
+const horizontalWin = function(){
+    if(board[0] !== null && board[0] === board[1] && board[0] === board[2]){
+      winner = board[0];
+    } else if (board[3] !== null && board[3] === board[4] && board[3] === board[5]){
+      winner = board[3];
+    } else if (board[6] !== null && board[6] === board[7] && board[6] === board[8]){
+      winner = board[6];
+    }
+  return winner;
 };
 //checks for horizontal set up for possible win
 
-const verticalAdjacent = function(firstCell, adjacentCellOne, adjacentCellTwo){
-  if(Math.abs(adjacentCellOne - firstCell) === 3){
-    if(Math.abs(adjacentCellTwo - firstCell) === 3 || Math.abs(adjacentCellTwo - firstCell) === 6){
-      return true;
-    } else {
-      return false;
+
+const verticalWin = function(){
+    if(board[0] !== null && board[0] === board[3] && board[0] === board[6]){
+      winner = board[0];
+    } else if (board[1] !== null && board[1] === board[4] && board[1] === board[7]){
+      winner = board[1];
+    } else if (board[2] !== null && board[2] === board[5] && board[2] === board[8]){
+      winner = board[2];
     }
-  }
-  return false;
+  return winner;
 };
 //checks for vertical setup for possible win
 
-const diagonalAdjacent = function(firstCell, adjacentCellOne, adjacentCellTwo){
-  if (Math.abs(adjacentCellOne - firstCell) === 4){
-    if(Math.abs(adjacentCellTwo - firstCell === 4) || Math.abs(adjacentCellTwo - firstCell === 8)){
+const diagonalWin = function(){
+  if (board[4] !== null){
+    if (board[4] === board[0] && board[4] === board[8] || board[4] === board[2] && board[4] === board[6]){
+      winner = board[4];
+    }
+  }
+  return winner;
+};
+//checks for diagonal setup for possible win
+
+const winCheck = function(firstCell, adjacentCellOne, adjacentCellTwo){
+  if(xCount >= 3 || oCount >= 3){
+    if (horizontalWin() || verticalWin() || diagonalWin()){
       return true;
     } else {
       return false;
     }
   }
-  return false;
-};
-//checks for diagonal setup for possible win
-
-const sameThree = function(firstCell, adjacentCellOne, adjacentCellTwo){
-  return (board[firstCell] === 'x' && board[adjacentCellOne] === 'x' && board[adjacentCellTwo] === 'x') ||
-  (board[firstCell] === 'o' && board[adjacentCellOne] === 'o' && board[adjacentCellTwo] === 'o');
-};
-//checks if the same three tokens are in adjacent cells
-
-const winCheck = function(firstCell, adjacentCellOne, adjacentCellTwo){
-  if (sameThree(firstCell, adjacentCellOne, adjacentCellTwo)){
-    if(horizontalAdjacent(firstCell, adjacentCellOne, adjacentCellTwo) ||
-      verticalAdjacent(firstCell, adjacentCellOne, adjacentCellTwo) ||
-      diagonalAdjacent(firstCell, adjacentCellOne, adjacentCellTwo)){
-        return true;
-      } else {
-        return false;
-      }
-  }
-  return false;
 };
 //checks if a player has won
 
